0c186e72a8fbf7bd2c19f07c7841aa2daa2af6aa
Dylan Clements <dclements@protonmail.com>
2019-01-27 19:55:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,11 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  #text.scan(/un\w+ing/i)
-  test_array = []
-  test_array << text.scan(/un\z+ing/i)
-  #test_array << text.scan(/un\w+ing/i)
-  binding.pry
+  text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
