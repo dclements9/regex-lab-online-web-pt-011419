d3a2d6cc1d6fc2b30c098afd39e452d82b0a9e86
Dylan Clements <dclements@protonmail.com>
2019-01-27 19:14:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  vowel_words = []
+  vowel_words << word.scan(/[aeiou]/)
+  binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
